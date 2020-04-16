14a8e85547f866e7cc8b3f85836677ea63f7d960
lsj <lsj@foxhis.local>
2020-04-16 13:29:11 +0800

    nginx 调整，增加对后端的nginx跳转

--- docker/config.prod.ts
+++ docker/config.prod.ts
@@ -1,5 +1,5 @@
 const config: IConfig = {
-  serve: `http://${window.location.hostname}:38080`,
+  serve: `http://${window.location.hostname}/api`,
   keys: ['some secret hurr'],
   session: {
     key: 'koa:sess',
